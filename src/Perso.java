8cb6a09472bdff6f70af5a25e37c8cde29c69c35
Julien Desmons <julien.desmons2.etu@univ-lille.fr>
2020-08-31 14:45:12 +0200

    Base perso

--- src/Perso.java
+++ src/Perso.java
@@ -1,4 +1,26 @@
 
-public class Perso {
+public abstract class Perso {
+	protected int attack;
+	protected int HP;
+	protected int deplacement;
+	
+	public Perso() {
+		
+	}
 
+	public int getAttack() {
+		return attack;
+	}
+
+	public int getHP() {
+		return HP;
+	}
+
+	public int getDeplacement() {
+		return deplacement;
+	}
+	
+	public void wounded(int damage) {
+		this.HP = this.HP - damage;
+	}
 }
