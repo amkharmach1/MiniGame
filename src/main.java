b3becaa0841c59e0ed5c42e9390f07cb4636e963
bellefel <louis.bellefemine.etu@univ-lille.fr>
2020-09-01 09:19:47 +0200

    demo

--- src/main.java
+++ src/main.java
@@ -3,6 +3,7 @@ public class main {
 
 	public static void main(String[] args) {
 		Plateau t = new Plateau(8, 8);
+		mouvement move = new mouvement();
 		Perso l = new Leader(1,1);
 		Perso s = new Sniper(2,2);
 		Perso T = new Tank(3,3);
@@ -11,7 +12,13 @@ public class main {
 		t.putPersoInPlateau(s);
 		t.putPersoInPlateau(T);
 		t.putPersoInPlateau(e);
-		System.out.println(t);
+		for(int i = 0; i < 10; i++) {
+			System.out.flush();
+			System.out.println(t);
+			System.out.println(move.getMouvement());
+			
+		}
+
 
 	}
 
