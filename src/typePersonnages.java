3dd38c9fa7f79bb4295c6716494d8f90babea44f
Rayd Tarafi <rayd.tarafi.etu@univ-lille.fr>
2020-09-01 15:45:54 +0200

    ajout "portée" dans les attributs des persos

--- src/typePersonnages.java
+++ src/typePersonnages.java
@@ -1,16 +1,18 @@
 
 public enum typePersonnages {
 	
-	TANK(2,8,1), SNIPER(5,2,2), LEADER(1,5,2), ECLAIREUR(2,2,5);
+	TANK(2,8,1,1), SNIPER(5,2,2,2), LEADER(1,5,2,1), ECLAIREUR(2,2,5,1);
 	
 	public int attaque;
 	public int hp;
 	public int deplacement;
+	public int portee;
 	
-	private typePersonnages(int attaque, int hp, int deplacement) {
+	private typePersonnages(int attaque, int hp, int deplacement, int portee) {
 		this.attaque = attaque;
 		this.hp = hp;
 		this.deplacement = deplacement;
+		this.portee = portee;
 	}
 	
 }
