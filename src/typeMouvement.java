36e46c919ad3271321de9c10e667398929a16d74
Rayd Tarafi <rayd.tarafi.etu@univ-lille.fr>
2020-09-01 10:57:19 +0200

    ajout coordonnées dans l'enum

--- src/typeMouvement.java
+++ src/typeMouvement.java
@@ -1,4 +1,21 @@
 
 public enum typeMouvement {
-	HAUT, BAS, GAUCHE, DROITE;
+	
+	HAUT(0,-1), BAS(0,1), GAUCHE(-1,0), DROITE(1,0);
+	
+	public int x;
+	public int y;
+	
+	private typeMouvement(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+	
+	public int getX() {
+		return this.x;
+	}
+	
+	public int  getY() {
+		return this.y;
+	}
 }
