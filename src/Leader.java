6009a00289f20e5ec1e2ebf3110d137b13e4b590
Julien Desmons <julien.desmons2.etu@univ-lille.fr>
2020-09-02 08:48:10 +0200

    Ajout découvrir, récuperer et drop le drapeau

--- src/Leader.java
+++ src/Leader.java
@@ -1,9 +1,39 @@
 public class Leader extends Perso{
+	
 	public Leader(int x, int y, Plateau lePlateau) {
 		super(typePersonnages.LEADER, x, y, lePlateau);
 	}
 
 	public String toString() {
 		return "👑";
+	}
+	
+	public void wounded(int damage) {
+		System.out.println(this.toString() + " a perdu " + damage + " HP.");
+		this.HP = this.HP - damage;
+		if(!isAlive()) {
+			this.lePlateau.plateau[x][y].setElement(null);
+			if(this.hasFlag) {
+				this.lePlateau.plateau[x][y].setElement(new Flag(x,y));
+			}
+		}
+	}
+	
+	public void competance() {
+		boolean flagAround = false;
+		typeMouvement direction;
+		int i = 0;
+		do {
+			direction = typeMouvement.values()[i];
+			Element e = this.lePlateau.plateau[direction.getX() + x][direction.getY() + y].getElement();
+			if (e.isFlag()) {
+				flagAround = true;
+				Flag f = (Flag) e;
+				f.setIsTakenBy(this);
+				this.hasFlag = true;
+			} else {
+				i++;
+			}
+		} while (i < 4 && !flagAround);
 	}
 }
